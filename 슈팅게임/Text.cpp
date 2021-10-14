caea3f1fe9fc52b9f5adc37b023d493851bfaa4c
성하천 <shc0073@gmail.com>
2021-10-14 20:54:01 +0900

    2021년 10월 14일 9시

--- 슈팅게임/Text.cpp
+++ 슈팅게임/Text.cpp
@@ -35,11 +35,15 @@ void Text::start()
     }
 
     //폰트..크기 지정하기//
-    FT_Set_Pixel_Sizes(face, 32, 32);
+    FT_Set_Pixel_Sizes(face, 64, 64);
 }
 
 void Text::draw()
 {
+    //글자 출력위치 변수
+    float fontx = getPx();
+    float fonty = getPy();
+
     //글자 폰트를 face에서 찾아오기
     wstring text = L"점수";
 
@@ -48,10 +52,10 @@ void Text::draw()
         int index = FT_Get_Char_Index(face, text[i]);
 
         //font 정보를..출력가능한 이미지 형태로..변환하기
-        FT_Load_Glyph(face, index, FT_LOAD_DEFAULT);        //글자정보(서체 - Glyph)로드하기
+        FT_Load_Glyph  (face, index, FT_LOAD_DEFAULT);       //글자정보(서체 - Glyph)로드하기
         FT_Render_Glyph(face->glyph, FT_RENDER_MODE_NORMAL); //글자정보를 이미지로 변환하기 
 
-        int width = face->glyph->bitmap.width; //이미지 가로크기
+        int width  = face->glyph->bitmap.width; //이미지 가로크기
         int height = face->glyph->bitmap.rows;  //이미지 세로크기
         unsigned char* buffer = face->glyph->bitmap.buffer; //이미지 데이타 버퍼
 
@@ -63,12 +67,28 @@ void Text::draw()
 
                 if (value > 0)  //폰트 이미지 출력하기
                 {
+                    //알파블렌딩//
+                    float alpha = value / 255.0f;  //알파값을 ( 0 ~ 1.0 사이로..정규화)
+
+                    //배경색 가져오기//
+                    unsigned char R, G, B;
+                    getPixel(fontx + x, fonty + y, &R, &G, &B);
+
                     //폰트 색상//
                     unsigned char r=255, g=0, b=0;
 
-                    setPixel(x, y, r, g, b);
+                    //알파블렌딩 공식적용하기
+                    unsigned char _r = r * alpha + R * (1 - alpha);
+                    unsigned char _g = g * alpha + G * (1 - alpha);
+                    unsigned char _b = b * alpha + B * (1 - alpha);
+
+                    setPixel(fontx+x, fonty+y, _r, _g, _b);
                 }
             }            
         }
+
+        //다음 글자출력 위치로..이동하기//
+        fontx = fontx + face->glyph->advance.x/64;
+        fonty = fonty + face->glyph->advance.y/64;
     }
 }
